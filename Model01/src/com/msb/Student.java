26caa355df26e219ae1e21090277bab9fdc1bc33
adaDevC <jenny_devc@outlook.com>
2024-03-29 09:43:11 -0400

    This is the first deliver for the project name "Github practise-Model01"

--- Model01/src/com/msb/Student.java
+++ Model01/src/com/msb/Student.java
@@ -2,4 +2,21 @@ package com.msb;
 
 public class Student {
 
+    private String name="Jenny";
+
+    public Student() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        System.out.println(s);
+    }
 }
